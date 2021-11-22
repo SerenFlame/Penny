569199ca198b4879d20c77dc95cd4bc1590e5727
SerenFlame <thomasjko2002@gmail.com>
2021-11-23 00:05:58 +0800

    First

---
+++ announce.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require('Discord.js')
+module.exports = {
+    name: "annouce"
+    description: "Get the bot to say what you want in a specific channel"
+    usage "<channel id> <msg>"
+    run: async(bot,message,args) => {
+        let rChannel =  message.guild.channels.cache.get.(args[0])
+        if(!rChannel)return message.channel.send(`You did not specify where to annouce!`)
+        let MSG = message.content.split(`${bot.prefix}announce ${rChannel} `).join("")
+        if(!MSG)return message.channel.send(`You did not specify what you announcement contains.`)
+        const em = new MessageEmbed()
+        .setTitle(`New Announcement`)
+        .setDescription(`${MSG}`)
+        .setColor('BLUE')
+        rChannel.send(em)
+        message.delete()
+    }
+}
