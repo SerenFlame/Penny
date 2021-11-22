1c09429fbd8d8bcd17e8b3b6b17c3dbff0a2e8c6
SerenFlame <thomasjko2002@gmail.com>
2021-11-23 00:50:04 +0800

    Update announce.js

--- announce.js
+++ announce.js
@@ -3,11 +3,11 @@ module.exports = {
     name: "announce"
     description: "Get the bot to say what you want in a specific channel"
     usage "<channel id> <msg>"
-    async(bot,message,args) => {
-        let rChannel =  message.guild.channels.cache.get.(args[0])
+    run: async(bot,message,args) => {
+        const rChannel =  message.guild.channels.cache.get.(args[0])
         if(!rChannel)return message.channel.send(`You did not specify where to annouce!`)
         console.log(rChannel)
-        let MSG = message.content.split(`${bot.prefix}announce ${rChannel.id} `).join("")
+        const MSG = message.content.split(`${bot.prefix}announce ${rChannel.id} `).join("")
         if(!MSG)return message.channel.send(`You did not specify what you announcement contains.`)
         const em = new MessageEmbed()
         .setTitle(`New Announcement`)
