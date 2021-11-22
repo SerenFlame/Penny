cc853f0fc2dd7cb9fd9973f60b6eb2f5f7fcadcc
SerenFlame <thomasjko2002@gmail.com>
2021-11-23 01:25:33 +0800

    Update announce.js

--- announce.js
+++ announce.js
@@ -1,21 +1,10 @@
-const { MessageEmbed } = require('Discord.js')
-module.exports = {
-    const prefix = "!";
-    
-    name: "announce",
-    description: "Get the bot to say what you want in a specific channel",
-    usage "<channel id> <msg>",
-    run: async(bot,message,args)=>{
-        const rChannel =  message.guild.channels.cache.get.(args[0])
-        if(!rChannel)return message.channel.send(`You did not specify where to annouce!`)
-        console.log(rChannel)
-        const MSG = message.content.split(`${bot.prefix}announce ${rChannel.id} `).join("")
-        if(!MSG)return message.channel.send(`You did not specify what you announcement contains.`)
-        const em = new MessageEmbed()
-        .setTitle(`New Announcement`)
-        .setDescription(`${MSG}`)
-        .setColor('BLUE')
-        rChannel.send(em)
-        message.delete()
-    }
-}   
+module.exports = (client) => {
+    client.on('message', (message) => {
+      const { channel } = message
+  
+      if (channel.type === 'news') {
+        message.crosspost()
+        console.log('published news message')
+      }
+    })
+  }
