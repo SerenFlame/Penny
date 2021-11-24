c553614e30610de0a6b8907c5c16a1fd83bc1e70
SerenFlame <thomasjko2002@gmail.com>
2021-11-24 15:01:40 +0800

    a

--- commands/welcome.js
+++ commands/welcome.js
@@ -7,10 +7,10 @@ client.on("guildMemberAdd", (member) => {
     const channel = member.guild.channels.cache.get(channelID);
 
         const newEmbed = new MessageEmbed()
-            .setTitle (`Welcome to ${member.guild.name}`)
+            .setTitle (`You finally arrived at ${member.guild.name}`)
             .setThumbnail(member.user.displayAvatarURL({dynamic: true, size: 512}))
-            .setDescription (`Welcome to Discord Tambayan, <@${member.user.id}>. I am Penny, nice to meet you!`)
-            .setFooter (`Welcome` ,member.user.displayAvatarURL({dynamic: true, size:512}))
+            .setDescription (`Welcome to the server, <@${member.user.id}>. I am Penny, nice to meet you!`)
+            .setFooter (`Welcome and hope you enjoy the place!` ,member.user.displayAvatarURL({dynamic: true, size:512}))
             .setColor('RANDOM')
         
     channel.send({ embeds: [newEmbed] })
