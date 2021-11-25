39b35e35ed3a33dbd64a2ae990a93ab76e2797d0
SerenFlame <thomasjko2002@gmail.com>
2021-11-26 00:22:01 +0800

    a

---
+++ commands/role-setup.js
@@ -0,0 +1,11 @@
+const EditMessage = require('../../utils/EditMessage')
+
+module.exports = (client) => {
+    EditMessage(client, '870135591835168778', new MessageEmbed()
+    .setTitle('Reaction Roles')
+    .setColor('RANDOM')
+    .setDescription('Claim roles using reactions.')
+    .setFooter('Reaction Roles')
+    , ['<:HI:913463532576731207>', '<:LOVESTRUCK:913463533990182912>', '<:PISSED:913463533923090473>', '<:POGIPOINTS:913463533923086376>', '<:SAD:913463533272969216>', '<:STRESSED:913463533067452436>' 
+])
+}
