fe2cc41a1606889955502604eac8c0f0e4342f4b
SerenFlame <thomasjko2002@gmail.com>
2021-11-26 02:58:50 +0800

    a

--- Reaction Roles/setup.js
+++ Reaction Roles/setup.js
@@ -1,35 +1,21 @@
-const Discord = require("discord.js")
-const { MessageButton, MessageActionRow } = require("discord-buttons")
+const { Client, Message, MessageActionRow, MessageButton } = require('discord.js');
 
-module.exports = async (client, messageCreate, args, prefix) => {
-
-    const embed = new Discord.MessageEmbed()
-    .setTitle("Pick a role, any role")
-    .setDescription("Pick a role to access the channels.")
-    .setColor("NAVY")
-    
-    const bt1 = new MessageButton()
-    .setStyle("green")
-    .setLabel("Yes")
-    .setID("1")
-
-    const bt2 = new MessageButton()
-    .setStyle("green")
-    .setLabel("No")
-    .setID("2")
-
-    const yes = new MessageActionRow()
-    .addComponent(btn1)
-    .addComponent(btn2)
-
-    messageCreate.channel.send('Hello', {
-        embeds: embed, 
-        component: yes
-    })
-
-}
-
-module.exports.help = {
-    name: "rr"
-    aliases: []
+module.exports = { 
+    name: "role"   
+    description: "Pick any role to access more of the server."
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async {client, message, args} => {
+        const row = new MessageActionRow().addComponents(
+            new MessageButton(
+                .setCustomId("random")
+                .setLabel("primary")
+                .setStyle("PRIMARY")
+            )
+        );
+        messageCreate.channel.send({content: "Hello World", components: []})
+    }
 }
