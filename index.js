4fb2e05a866f61f72530556584ccb272c2d5306a
SerenFlame <thomasjko2002@gmail.com>
2021-11-26 01:07:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+const client = new Discord.Client({
     
     allowedMentions: {
         parse: ['users', 'roles'],
